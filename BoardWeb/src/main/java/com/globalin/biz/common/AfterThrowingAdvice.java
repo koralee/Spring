a2fda004c3d8cfd3ae6a229b192ce4a6861d3fb0
LEE SUHONG <tnghd222@naver.com>
2023-03-07 14:24:13 +0900

    Spring

---
+++ BoardWeb/src/main/java/com/globalin/biz/common/AfterThrowingAdvice.java
@@ -0,0 +1,24 @@
+package com.globalin.biz.common;
+
+import org.aspectj.lang.JoinPoint;
+
+public class AfterThrowingAdvice {
+
+	public void exceptionLog(JoinPoint jp, Exception exceptionObj) {
+		
+		String method = jp.getSignature().getName();
+		
+		/*
+		System.out.println("[예외처리]" + method +"() 메소드 실행 중 발생된 예외 메시지 : " + exceptionObj.getMessage());
+		*/
+		
+		System.out.println(method+"() 메소드 실행 중 예외발생 !!!!");
+		if(exceptionObj instanceof IllegalArgumentException) {
+			System.out.println("부적합한 값이 입력되었습니다.");
+		}else if(exceptionObj instanceof NumberFormatException) {
+			System.out.println("숫자 형식의 값이 아닙니다.");
+		}else if(exceptionObj instanceof Exception) {
+			System.out.println("문제 발생");
+		}
+	}
+}
